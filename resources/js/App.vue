b4596883510f37123944484cb7b6755e6d75caec
Amphavanh <amphavanh_ben@yahoo.com>
2022-02-28 13:46:26 +0700

    update v2

---
+++ resources/js/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>vue app</h1>
+        <router-view></router-view>
+       
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos02App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/pages/Store.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Store</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos02Store',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
